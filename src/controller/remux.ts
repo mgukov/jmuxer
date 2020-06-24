dfcbc0d2a67e2953f06040d87ae9c361c0aff44f
Michael Gukov <mgookov@gmail.com>
2020-06-24 20:14:59 +0500

    debug

--- src/controller/remux.ts
+++ src/controller/remux.ts
@@ -92,6 +92,9 @@ export default class RemuxController extends Event {
                     let pay = muxer.getPayload();
                     if (pay && pay.byteLength) {
                         const moof = Mp4.moof(muxer.seq, muxer.dts, muxer.mp4track);
+
+                        console.log(JSON.stringify(moof.dump()));
+
                         const mdat = Mp4.mdat(pay);
                         let payload = Utils.appendByteArray(moof.getData(), mdat.getData());
                         let data = {
